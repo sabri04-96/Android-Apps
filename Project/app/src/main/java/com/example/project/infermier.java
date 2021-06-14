7cf22aa7ff6810fa19cfb6527fe3d72aeafe06df
sabri04-96 <sabri.fetoui@tek-up.de>
2021-06-15 00:18:34 +0200

    1.0
    first version

---
+++ Project/app/src/main/java/com/example/project/infermier.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infermier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infermier);
+    }
+}
